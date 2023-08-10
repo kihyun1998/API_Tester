2e3ba57cb8b6822ad8c8e6cb68c6a5f3b7a6b6d8
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-10 09:40:31 +0900

    창 사이즈 변환 기능 추가

--- API_Tester/Form1.Designer.cs
+++ API_Tester/Form1.Designer.cs
@@ -29,13 +29,13 @@ namespace API_Tester
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.cBoxMethod = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.URL = new System.Windows.Forms.Label();
             this.btnRequest = new System.Windows.Forms.Button();
             this.lblMsg = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.tBoxRst = new System.Windows.Forms.TextBox();
@@ -47,7 +47,10 @@ namespace API_Tester
             this.tBoxMsg = new API_Tester.Custom.CustomTextBox();
             this.tBoxCookie = new API_Tester.Custom.CustomTextBox();
             this.tBoxURL = new API_Tester.Custom.CustomTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.titleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -56,9 +59,9 @@ namespace API_Tester
             this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
             this.label1.Font = new System.Drawing.Font("궁서", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(140, 109);
+            this.label1.Location = new System.Drawing.Point(29, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(246, 40);
+            this.label1.Size = new System.Drawing.Size(201, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "API Tester";
             // 
@@ -67,9 +70,10 @@ namespace API_Tester
             this.cBoxMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cBoxMethod.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cBoxMethod.FormattingEnabled = true;
-            this.cBoxMethod.Location = new System.Drawing.Point(238, 260);
+            this.cBoxMethod.Location = new System.Drawing.Point(112, 157);
+            this.cBoxMethod.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cBoxMethod.Name = "cBoxMethod";
-            this.cBoxMethod.Size = new System.Drawing.Size(121, 23);
+            this.cBoxMethod.Size = new System.Drawing.Size(106, 20);
             this.cBoxMethod.TabIndex = 1;
             this.cBoxMethod.TabStop = false;
             this.cBoxMethod.SelectedIndexChanged += new System.EventHandler(this.cBoxMethod_SelectedIndexChanged);
@@ -79,9 +83,9 @@ namespace API_Tester
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("궁서체", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(152, 261);
+            this.label2.Location = new System.Drawing.Point(38, 158);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 19);
+            this.label2.Size = new System.Drawing.Size(55, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Method";
             // 
@@ -90,9 +94,9 @@ namespace API_Tester
             this.URL.AutoSize = true;
             this.URL.Font = new System.Drawing.Font("궁서체", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.URL.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.URL.Location = new System.Drawing.Point(440, 261);
+            this.URL.Location = new System.Drawing.Point(235, 157);
             this.URL.Name = "URL";
-            this.URL.Size = new System.Drawing.Size(39, 19);
+            this.URL.Size = new System.Drawing.Size(31, 15);
             this.URL.TabIndex = 3;
             this.URL.Text = "URL";
             // 
@@ -100,9 +104,10 @@ namespace API_Tester
             // 
             this.btnRequest.Font = new System.Drawing.Font("궁서체", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btnRequest.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnRequest.Location = new System.Drawing.Point(1122, 309);
+            this.btnRequest.Location = new System.Drawing.Point(864, 198);
+            this.btnRequest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRequest.Name = "btnRequest";
-            this.btnRequest.Size = new System.Drawing.Size(118, 49);
+            this.btnRequest.Size = new System.Drawing.Size(130, 49);
             this.btnRequest.TabIndex = 5;
             this.btnRequest.Text = "Request";
             this.btnRequest.UseVisualStyleBackColor = true;
@@ -113,26 +118,19 @@ namespace API_Tester
             this.lblMsg.AutoSize = true;
             this.lblMsg.Font = new System.Drawing.Font("궁서체", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.lblMsg.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblMsg.Location = new System.Drawing.Point(151, 354);
+            this.lblMsg.Location = new System.Drawing.Point(46, 251);
             this.lblMsg.Name = "lblMsg";
-            this.lblMsg.Size = new System.Drawing.Size(59, 19);
+            this.lblMsg.Size = new System.Drawing.Size(47, 15);
             this.lblMsg.TabIndex = 8;
             this.lblMsg.Text = "msg =";
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.panel1.Location = new System.Drawing.Point(150, 399);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1090, 10);
-            this.panel1.TabIndex = 9;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.panel2.Location = new System.Drawing.Point(150, 205);
+            this.panel2.Location = new System.Drawing.Point(37, 102);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1090, 10);
+            this.panel2.Size = new System.Drawing.Size(957, 10);
             this.panel2.TabIndex = 10;
             // 
             // label3
@@ -140,19 +138,20 @@ namespace API_Tester
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("궁서체", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Location = new System.Drawing.Point(121, 309);
+            this.label3.Location = new System.Drawing.Point(22, 206);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 19);
+            this.label3.Size = new System.Drawing.Size(71, 15);
             this.label3.TabIndex = 12;
             this.label3.Text = "Cookie =";
             // 
             // tBoxRst
             // 
-            this.tBoxRst.Location = new System.Drawing.Point(147, 453);
+            this.tBoxRst.Location = new System.Drawing.Point(35, 332);
+            this.tBoxRst.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tBoxRst.Multiline = true;
             this.tBoxRst.Name = "tBoxRst";
             this.tBoxRst.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tBoxRst.Size = new System.Drawing.Size(1084, 223);
+            this.tBoxRst.Size = new System.Drawing.Size(961, 198);
             this.tBoxRst.TabIndex = 13;
             // 
             // titleBar
@@ -165,8 +164,9 @@ namespace API_Tester
             this.titleBar.Controls.Add(this.btnMin);
             this.titleBar.Controls.Add(this.btnX);
             this.titleBar.Location = new System.Drawing.Point(0, -2);
+            this.titleBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.titleBar.Name = "titleBar";
-            this.titleBar.Size = new System.Drawing.Size(1275, 45);
+            this.titleBar.Size = new System.Drawing.Size(1027, 42);
             this.titleBar.TabIndex = 14;
             this.titleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.titleBar_MouseDown);
             this.titleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.titleBar_MouseMove);
@@ -179,9 +179,10 @@ namespace API_Tester
             this.btnNom.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnNom.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btnNom.ForeColor = System.Drawing.Color.White;
-            this.btnNom.Location = new System.Drawing.Point(1180, 5);
+            this.btnNom.Location = new System.Drawing.Point(922, 5);
+            this.btnNom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnNom.Name = "btnNom";
-            this.btnNom.Size = new System.Drawing.Size(41, 37);
+            this.btnNom.Size = new System.Drawing.Size(36, 30);
             this.btnNom.TabIndex = 18;
             this.btnNom.Text = "+";
             this.btnNom.UseVisualStyleBackColor = false;
@@ -194,9 +195,10 @@ namespace API_Tester
             this.btnMax.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnMax.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btnMax.ForeColor = System.Drawing.Color.White;
-            this.btnMax.Location = new System.Drawing.Point(1180, 5);
+            this.btnMax.Location = new System.Drawing.Point(922, 5);
+            this.btnMax.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnMax.Name = "btnMax";
-            this.btnMax.Size = new System.Drawing.Size(41, 37);
+            this.btnMax.Size = new System.Drawing.Size(36, 30);
             this.btnMax.TabIndex = 15;
             this.btnMax.Text = "ㅁ";
             this.btnMax.UseVisualStyleBackColor = false;
@@ -209,9 +211,10 @@ namespace API_Tester
             this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnMin.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btnMin.ForeColor = System.Drawing.Color.White;
-            this.btnMin.Location = new System.Drawing.Point(1137, 5);
+            this.btnMin.Location = new System.Drawing.Point(875, 5);
+            this.btnMin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnMin.Name = "btnMin";
-            this.btnMin.Size = new System.Drawing.Size(41, 37);
+            this.btnMin.Size = new System.Drawing.Size(36, 30);
             this.btnMin.TabIndex = 15;
             this.btnMin.Text = "ㅡ";
             this.btnMin.UseVisualStyleBackColor = false;
@@ -224,9 +227,10 @@ namespace API_Tester
             this.btnX.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnX.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btnX.ForeColor = System.Drawing.Color.White;
-            this.btnX.Location = new System.Drawing.Point(1223, 5);
+            this.btnX.Location = new System.Drawing.Point(970, 5);
+            this.btnX.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnX.Name = "btnX";
-            this.btnX.Size = new System.Drawing.Size(41, 37);
+            this.btnX.Size = new System.Drawing.Size(36, 30);
             this.btnX.TabIndex = 15;
             this.btnX.Text = "X";
             this.btnX.UseVisualStyleBackColor = false;
@@ -237,12 +241,13 @@ namespace API_Tester
             this.tBoxMsg.BackColor = System.Drawing.Color.WhiteSmoke;
             this.tBoxMsg.BorderColor = System.Drawing.Color.Gray;
             this.tBoxMsg.BorderSize = 2;
-            this.tBoxMsg.Location = new System.Drawing.Point(238, 354);
+            this.tBoxMsg.Location = new System.Drawing.Point(112, 251);
+            this.tBoxMsg.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tBoxMsg.Multiline = false;
             this.tBoxMsg.Name = "tBoxMsg";
-            this.tBoxMsg.Padding = new System.Windows.Forms.Padding(7);
+            this.tBoxMsg.Padding = new System.Windows.Forms.Padding(6);
             this.tBoxMsg.PasswordChar = false;
-            this.tBoxMsg.Size = new System.Drawing.Size(801, 32);
+            this.tBoxMsg.Size = new System.Drawing.Size(735, 26);
             this.tBoxMsg.TabIndex = 18;
             this.tBoxMsg.UnderlinedStyle = true;
             // 
@@ -251,12 +256,13 @@ namespace API_Tester
             this.tBoxCookie.BackColor = System.Drawing.Color.WhiteSmoke;
             this.tBoxCookie.BorderColor = System.Drawing.Color.Gray;
             this.tBoxCookie.BorderSize = 2;
-            this.tBoxCookie.Location = new System.Drawing.Point(238, 309);
+            this.tBoxCookie.Location = new System.Drawing.Point(112, 206);
+            this.tBoxCookie.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tBoxCookie.Multiline = false;
             this.tBoxCookie.Name = "tBoxCookie";
-            this.tBoxCookie.Padding = new System.Windows.Forms.Padding(7);
+            this.tBoxCookie.Padding = new System.Windows.Forms.Padding(6);
             this.tBoxCookie.PasswordChar = false;
-            this.tBoxCookie.Size = new System.Drawing.Size(801, 32);
+            this.tBoxCookie.Size = new System.Drawing.Size(735, 26);
             this.tBoxCookie.TabIndex = 17;
             this.tBoxCookie.UnderlinedStyle = true;
             // 
@@ -265,21 +271,44 @@ namespace API_Tester
             this.tBoxURL.BackColor = System.Drawing.Color.WhiteSmoke;
             this.tBoxURL.BorderColor = System.Drawing.Color.Gray;
             this.tBoxURL.BorderSize = 2;
-            this.tBoxURL.Location = new System.Drawing.Point(503, 260);
+            this.tBoxURL.Location = new System.Drawing.Point(285, 158);
+            this.tBoxURL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tBoxURL.Multiline = false;
             this.tBoxURL.Name = "tBoxURL";
-            this.tBoxURL.Padding = new System.Windows.Forms.Padding(7);
+            this.tBoxURL.Padding = new System.Windows.Forms.Padding(6);
             this.tBoxURL.PasswordChar = false;
-            this.tBoxURL.Size = new System.Drawing.Size(737, 32);
+            this.tBoxURL.Size = new System.Drawing.Size(703, 26);
             this.tBoxURL.TabIndex = 16;
             this.tBoxURL.UnderlinedStyle = true;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Location = new System.Drawing.Point(37, 297);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(957, 10);
+            this.panel1.TabIndex = 19;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1004, 544);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(15, 15);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(1269, 757);
+            this.ClientSize = new System.Drawing.Size(1020, 560);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.tBoxMsg);
             this.Controls.Add(this.tBoxCookie);
             this.Controls.Add(this.tBoxURL);
@@ -287,7 +316,6 @@ namespace API_Tester
             this.Controls.Add(this.tBoxRst);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblMsg);
             this.Controls.Add(this.btnRequest);
             this.Controls.Add(this.URL);
@@ -296,10 +324,13 @@ namespace API_Tester
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(1020, 560);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.titleBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -313,7 +344,6 @@ namespace API_Tester
         private System.Windows.Forms.Label URL;
         private System.Windows.Forms.Button btnRequest;
         private System.Windows.Forms.Label lblMsg;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox tBoxRst;
@@ -325,6 +355,8 @@ namespace API_Tester
         private Custom.CustomTextBox tBoxURL;
         private Custom.CustomTextBox tBoxCookie;
         private Custom.CustomTextBox tBoxMsg;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- API_Tester/Form1.cs
+++ API_Tester/Form1.cs
@@ -15,15 +15,17 @@ namespace API_Tester
 {
     public partial class Form1 : Form
     {
-        
+
 
 
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
         }
 
-        
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -45,7 +47,7 @@ namespace API_Tester
 
             string sUrl = tBoxURL.Text.ToString();
             string sMethod = cBoxMethod.SelectedItem.ToString();
-            string sCookie= tBoxCookie.Text.ToString();
+            string sCookie = tBoxCookie.Text.ToString();
 
             if (sMethod == "GET")
             {
@@ -111,19 +113,18 @@ namespace API_Tester
 
 
 
-        /////////////////
-        ///패널 이동////
-        ///////////////
+        ///////////////////////
+        ///패널 이동 + 상단바
         bool mouseDown;
         int sizeX;
         int sizeY;
 
-        
+
 
         private void titleBar_MouseDown(object sender, MouseEventArgs e)
         {
             mouseDown = true;
-            sizeX = e.X+20;
+            sizeX = e.X + 20;
             sizeY = e.Y;
         }
 
@@ -162,6 +163,63 @@ namespace API_Tester
         private void btnMin_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+        ////////////////////////////////////////////////////////////
+
+
+        ////////////////////
+        ///화면 사이즈 변경
+        
+        private const int
+            HTLEFT = 10,
+            HTRIGHT = 11,
+            HTTOP = 12,
+            HTTOPLEFT = 13,
+            HTTOPRIGHT = 14,
+            HTBOTTOM = 15,
+            HTBOTTOMLEFT = 16,
+            HTBOTTOMRIGHT = 17;
+
+        const int ten = 10;
+
+        //Rectangle Top { get { return new Rectangle(0, 0, this.ClientSize.Width, ten); } }
+        //Rectangle Left { get { return new Rectangle(0, 0, ten, this.ClientSize.Height); } }
+        Rectangle Bottom { get { return new Rectangle(0, this.ClientSize.Height - ten, this.ClientSize.Width, ten); } }
+        Rectangle Right { get { return new Rectangle(this.ClientSize.Width - ten, 0, ten, this.ClientSize.Height); } }
+
+        //Rectangle TopLeft { get { return new Rectangle(0, 0, ten, ten); } }
+        //Rectangle TopRight { get { return new Rectangle(this.ClientSize.Width - ten, 0, ten, ten); } }
+        //Rectangle BottomLeft { get { return new Rectangle(0, this.ClientSize.Height - ten, ten, ten); } }
+        Rectangle BottomRight { get { return new Rectangle(this.ClientSize.Width - ten, this.ClientSize.Height - ten, ten, ten); } }
+
+        // 창 변환 구간 함수
+        protected override void OnPaint(PaintEventArgs e) // you can safely omit this method if you want
+        {
+            //e.Graphics.FillRectangle(Brushes.White, Top);
+            //e.Graphics.FillRectangle(Brushes.White, Left);
+            e.Graphics.FillRectangle(Brushes.White, Right);
+            e.Graphics.FillRectangle(Brushes.White, Bottom);
+        }
+
+        // 커서 변경 함수
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if (message.Msg == 0x84) // WM_NCHITTEST
+            {
+                var cursor = this.PointToClient(Cursor.Position);
+
+                //if (TopLeft.Contains(cursor)) message.Result = (IntPtr)HTTOPLEFT;
+                //else if (TopRight.Contains(cursor)) message.Result = (IntPtr)HTTOPRIGHT;
+                //if (BottomLeft.Contains(cursor)) message.Result = (IntPtr)HTBOTTOMLEFT;
+                if (BottomRight.Contains(cursor)) message.Result = (IntPtr)HTBOTTOMRIGHT;
+
+                //else if (Top.Contains(cursor)) message.Result = (IntPtr)HTTOP;
+                //else if (Left.Contains(cursor)) message.Result = (IntPtr)HTLEFT;
+                else if (Right.Contains(cursor)) message.Result = (IntPtr)HTRIGHT;
+                else if (Bottom.Contains(cursor)) message.Result = (IntPtr)HTBOTTOM;
+            }
         }
     }
 }
